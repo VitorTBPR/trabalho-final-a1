94f2f101e3397991e08377122adcb57128dda16d
Vitor P Moreira <vipmoreira@hotmail.com>
2024-06-14 19:11:32 -0300

    feat: adicione classe Pergunta

---
+++ TrabalhoFinal/src/Pergunta.java
@@ -0,0 +1,26 @@
+public class Pergunta {
+    private String enunciado;
+    private String[] opcoes;
+    private char respostaCorreta;
+    
+    public Pergunta(String enunciado, String[] opcoes, char respostaCorreta) {
+        this.enunciado = enunciado;
+        this.opcoes = opcoes;
+        this.respostaCorreta = respostaCorreta;
+    }
+
+    public String getEnunciado() {
+        return enunciado;
+    }
+
+    public String[] getOpcoes() {
+        return opcoes;
+    }
+
+    public char getRespostaCorreta() {
+        return respostaCorreta;
+    }
+
+    
+    
+}
