5682da7f98785878596408deb4ee8a8a5c4c19bd
Vitor P Moreira <vipmoreira@hotmail.com>
2024-06-17 04:34:56 -0300

    Adicionado arraylist para Player com funções add, buscar, verificar e remover

--- TrabalhoFinal/src/Player.java
+++ TrabalhoFinal/src/Player.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Player {
     private String nome;
     private String senha;
@@ -20,8 +22,44 @@ public class Player {
         return nome + "," + senha;
     }
 
-    public static Player fromString(String playerData) {
-        String[] parts = playerData.split(",");
+    public static Player fromString(String playerDados) {
+        String[] parts = playerDados.split(",");
         return new Player(parts[0], parts[1]);
     }
+    
+
+    private static ArrayList<Player> listaPlayers = new ArrayList<>();
+
+    public static void salvarPlayer(Player player) {
+        listaPlayers.add(player);
+    }
+
+    public static ArrayList<Player> getListaPlayers() {
+        return listaPlayers;
+    }
+
+    public static void verificarListaVazia() throws Exception {
+
+        if (listaPlayers.isEmpty()) {
+            throw new Exception("\nNão há players cadastrados");
+        }
+
+    }
+
+    public static Player buscarPlayer(String nome) throws Exception {
+
+        for(Player tempPlayer : listaPlayers) {
+
+            if (tempPlayer.getNome().contains(nome)) {
+                return tempPlayer;
+            }
+        }
+
+        throw new Exception("Player " + nome + " não encontrado");
+    }
+
+    public static void apagarPlayer(Player player) {
+        listaPlayers.remove(player);
+    }
+
 }
